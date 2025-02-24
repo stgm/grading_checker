7360e07075fbdcff75566cd7e7aea5e17c90d620
Martijn Stegeman <martijn@stgm.nl>
2025-02-24 15:54:56 +0100

    first rubocop pass

--- lib/grading_checker.rb
+++ lib/grading_checker.rb
@@ -7,8 +7,8 @@ require "pathname"
 require "ripper"
 
 module GradingChecker
-    class Error < StandardError; end
 
+    class Error < StandardError; end
 
     class FileManager
         attr_reader :root_path
@@ -19,37 +19,38 @@ module GradingChecker
 
         def load_yaml(file_path)
             return {} unless file_path.exist?
+
             YAML.load_file(file_path, aliases: true) || {}
         rescue StandardError => e
             abort("Error loading YAML file: #{file_path}\n#{e.message}")
         end
 
         def find_grading_ymls
-            @root_path.find.select { |path| path.file? && path.basename.to_s == 'grading.yml' }
+            @root_path.find.select { |path| path.file? && path.basename.to_s == "grading.yml" }
         end
 
         def find_submit_ymls
-            @root_path.find.select { |path| path.file? && path.basename.to_s == 'submit.yml' }
+            @root_path.find.select { |path| path.file? && path.basename.to_s == "submit.yml" }
         end
 
         def merge_configs(grades1, grades2)
-            first_without_grades = grades1.reject { |k, _| ['templates', 'grades', 'calculation'].include?(k) }
-            second_without_grades = grades2.reject { |k, _| ['templates', 'grades', 'calculation'].include?(k) }
+            first_without_grades = grades1.reject { |k, _| %w[templates grades calculation].include?(k) }
+            second_without_grades = grades2.reject { |k, _| %w[templates grades calculation].include?(k) }
 
-            merged_grades = (grades1['grades'] || {}).merge(grades2['grades'] || {})
-            merged_calculation = (grades1['calculation'] || {}).merge(grades2['calculation'] || {})
+            merged_grades = (grades1["grades"] || {}).merge(grades2["grades"] || {})
+            merged_calculation = (grades1["calculation"] || {}).merge(grades2["calculation"] || {})
             merged_rest = first_without_grades.merge(second_without_grades)
-            merged_rest['grades'] = merged_grades
-            merged_rest['calculation'] = merged_calculation
+            merged_rest["grades"] = merged_grades
+            merged_rest["calculation"] = merged_calculation
 
             merged_rest
         end
     end
 
     class GradingYAMLValidator
-        VALID_GRADE_TYPES = ['pass', 'float', 'integer', 'points'].freeze
-        VALID_SUBGRADE_TYPES = ['integer', 'pass', 'boolean', 'float'].freeze
-        DEFAULT_GRADE_TYPE = 'float'
+        VALID_GRADE_TYPES = %w[pass float integer points].freeze
+        VALID_SUBGRADE_TYPES = %w[integer pass boolean float].freeze
+        DEFAULT_GRADE_TYPE = "float"
 
         GREEN = Rainbow(":)").green
         RED = Rainbow(":(").red
@@ -59,13 +60,14 @@ module GradingChecker
         end
 
         def validate_root_grading_config!
-            @root_config = @file_manager.load_yaml(@file_manager.root_path.join('grading.yml'))
-            validate_yaml_structure(@root_config, 'grading.yml')
+            @root_config = @file_manager.load_yaml(@file_manager.root_path.join("grading.yml"))
+            validate_yaml_structure(@root_config, "grading.yml")
         end
 
         def validate_subdir_configs!
             @file_manager.find_grading_ymls.each do |path|
                 next if path.dirname == @file_manager.root_path
+
                 merged_config = @file_manager.merge_configs(@root_config, @file_manager.load_yaml(path))
                 validate_yaml_structure(merged_config, path.relative_path_from(@file_manager.root_path).to_s)
             end
@@ -77,9 +79,9 @@ module GradingChecker
             submit_ymls = @file_manager.find_submit_ymls
             submit_ymls.each do |path|
                 submit_config = @file_manager.load_yaml(path)
-                next unless submit_config.is_a?(Hash) && submit_config.key?('name')
+                next unless submit_config.is_a?(Hash) && submit_config.key?("name")
 
-                submit_names[submit_config['name']] << path.relative_path_from(@file_manager.root_path).to_s
+                submit_names[submit_config["name"]] << path.relative_path_from(@file_manager.root_path).to_s
             end
 
             duplicate_submits = submit_names.select { |_, paths| paths.size > 1 }
@@ -87,7 +89,7 @@ module GradingChecker
             if duplicate_submits.any?
                 report_error("duplicate submit names found in submit.yml:")
                 duplicate_submits.each do |name, paths|
-                    puts "  - #{name} appears in: #{paths.join(', ')}"
+                    puts "  - #{name} appears in: #{paths.join(", ")}"
                 end
                 exit(1)
             else
@@ -96,20 +98,20 @@ module GradingChecker
         end
 
         def validate_yaml_structure(config, file_name)
-            validate_section(config, 'templates') do |templates|
+            validate_section(config, "templates") do |templates|
                 templates.each do |key, value|
-                    unless value.is_a?(Hash) && value.key?('type')
+                    unless value.is_a?(Hash) && value.key?("type")
                         report_error("template #{key} must be a hash with a 'type' key")
                         exit(1)
                     end
                 end
             end
 
-            validate_section(config, 'grades') do |grades|
+            validate_section(config, "grades") do |grades|
                 grades.each do |grade, details|
-                    grade_type = details['type'] || DEFAULT_GRADE_TYPE
+                    grade_type = details["type"] || DEFAULT_GRADE_TYPE
                     unless VALID_GRADE_TYPES.include?(grade_type)
-                        report_error("grade #{grade} has an invalid type: #{grade_type}. Must be one of #{VALID_GRADE_TYPES.join(', ')}")
+                        report_error("grade #{grade} has an invalid type: #{grade_type}. Must be one of #{VALID_GRADE_TYPES.join(", ")}")
                         exit(1)
                     end
                 end
@@ -117,7 +119,7 @@ module GradingChecker
 
             validate_module_definitions(config)
 
-            validate_section(config, 'calculation') do |calculation|
+            validate_section(config, "calculation") do |calculation|
                 calculation.each do |calc, details|
                     unless details.is_a?(Hash)
                         report_error("calculation #{calc} must be a hash of weighted components")
@@ -131,6 +133,7 @@ module GradingChecker
 
         def validate_section(config, section)
             return unless config.key?(section)
+
             unless config[section].is_a?(Hash)
                 report_error("#{section.capitalize} section must be a hash")
                 exit(1)
@@ -141,17 +144,17 @@ module GradingChecker
         def validate_module_definitions(config)
             return unless config.is_a?(Hash)
 
-            grade_keys = config['grades']&.keys || []
+            grade_keys = config["grades"]&.keys || []
 
             config.each do |key, value|
-                next unless value.is_a?(Hash) && value.key?('submits')
+                next unless value.is_a?(Hash) && value.key?("submits")
 
-                unless value['submits'].is_a?(Hash)
+                unless value["submits"].is_a?(Hash)
                     report_error("module definition #{key} has an invalid submits section: it must be a hash.")
                     exit(1)
                 end
 
-                value['submits'].each_key do |submit_key|
+                value["submits"].each_key do |submit_key|
                     unless grade_keys.include?(submit_key)
                         report_error("module definition #{key} references a non-existent grade: #{submit_key}")
                         exit(1)
@@ -168,6 +171,5 @@ module GradingChecker
             puts "#{RED} #{message}"
         end
     end
-
 
 end
--- lib/grading_checker/version.rb
+++ lib/grading_checker/version.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 module GradingChecker
-  VERSION = "0.1.0"
+
+    VERSION = "0.1.0"
+
 end
